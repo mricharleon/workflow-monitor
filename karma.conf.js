e661312f403f6689803776769caa484c82c61fcd
mricharleon <mricharleon@gmail.com>
2017-06-22 16:22:36 -0500

    test ci

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['Chrome
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
